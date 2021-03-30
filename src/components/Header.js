2e836f7dfc80f7956f233c4c0970c12f4d81c99c
mwinamijr <mwinamijr@gmail.com>
2021-03-30 15:59:26 +0300

    created basic look of my website frontend

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import SearchBox from './SearchBox'
+
+function Header() {
+
+    const logoutHandler = () => (
+        console.log("logout")
+    )
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Hayatul</Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <SearchBox />
+                        <Nav className="ml-auto">
+
+                            <LinkContainer to='/cart'>
+                                <Nav.Link ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            </LinkContainer>
+
+                            
+                                <NavDropdown title="Profile" id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+
+                                </NavDropdown>
+                            
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+                                    </LinkContainer>
+                              
+
+
+                            
+                                <NavDropdown title='Admin' id='adminmenue'>
+                                    <LinkContainer to='/admin/userlist'>
+                                        <NavDropdown.Item>Users</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/productlist'>
+                                        <NavDropdown.Item>Products</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/admin/orderlist'>
+                                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                                    </LinkContainer>
+
+                                </NavDropdown>
+                           
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+
+
+function HomeScreen({ history }) {
+  
+    return (
+        <div>
+            hayatul islamiya
+        </div>
+    )
+}
+
+export default HomeScreen
