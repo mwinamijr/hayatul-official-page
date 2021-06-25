77ebca525747f0fc6e55835965950df4ba00d301
mwinamijr <mwinamijr@gmail.com>
2021-06-25 06:31:39 +0300

    created redux store

---
+++ src/store.js
@@ -0,0 +1,70 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+    articlesListReducer,
+    articlesDetailsReducer,
+    articlesDeleteReducer,
+    articlesCreateReducer,
+    articlesUpdateReducer,
+} from './store/reducers/articlesReducers'
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailsReducer,
+    userUpdateProfileReducer,
+    userListReducer,
+    userDeleteReducer,
+    userUpdateReducer
+} from './store/reducers/userReducers'
+
+
+const reducer = combineReducers({
+    articlesList: articlesListReducer,
+    articlesDetails: articlesDetailsReducer,
+    articlesDelete: articlesDeleteReducer,
+    articlesCreate: articlesCreateReducer,
+    articlesUpdate: articlesUpdateReducer,
+
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+/*
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
+    orderList: orderListReducer,
+    orderDeliver: orderDeliverReducer,
+    */
+})
+
+
+const ArticlesListFromStorage = localStorage.getItem('articlesList') ?
+    JSON.parse(localStorage.getItem('articlesList')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+/*
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+    JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+*/
+const initialState = {
+    articles: {
+        articlesList: ArticlesListFromStorage,
+    },
+    userLogin: { userInfo: userInfoFromStorage },
+}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState,
+    composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
