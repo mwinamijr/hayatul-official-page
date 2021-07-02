5e43eb0db24282a7c64931b648e6302c2982b987
mwinamijr <mwinamijr@gmail.com>
2021-07-02 10:27:24 +0300

    Added new contents to the primary school

--- src/screens/education/PrimaryScreen.js
+++ src/screens/education/PrimaryScreen.js
@@ -7,14 +7,21 @@ function PrimaryScreen() {
       <div>
         <div className="px-4 my-5 text-center border-bottom">
           <h3 className="display-4 fw-bold">Shule ya Msingi Hayatul Islamiya(HISM)</h3>
-          <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <div className="col-lg-8 mx-auto">
+            <p className="lead mb-4">
+            Shule ya Msingi Hayatul islamiya ni shule bora kabisa inayofundisha, kuwajenga na kuwaandaa watoto kuwa tayari kuendelea elimu yao ya sekondari kwa mafanikio makubwa.
+                Kama ilivyo katika dhamira yetu, tunawalea na kwafundisha watoto wetu kwa <b>misingi na taratibu za dini ya kiislamu.</b> <br />
+              </p>
+              <p className="text-white rounded bg-dark">
+                <strong>Lugha yetu kuu ya kufundishia ni <em>Kiswahili</em></strong>
+
+              </p>
           </div>
         </div>
           <Row>
             <Col>
               <Card>
-                <Card.Img variant="top" src="http://127.0.0.1:8000/media/nursery/20201128_091608.jpg" />
+                <Card.Img variant="top" src="http://127.0.0.1:8000/images/nursery/20201128_091608.jpg" />
                 <Card.Body>
                 <Card.Title>Card title that wraps to a new line</Card.Title>
                 <Card.Text>
@@ -30,36 +37,33 @@ function PrimaryScreen() {
                 <Card.Body>
                   <Card.Title>Malezi</Card.Title>
                   <Card.Text>
-                      Kuanzia katika umri mdogo kabisa watoto wetu wanafundishwa malezi katika misingi na taratibu za dini ya kiislamu. 
-                      Malezi hayo yanahusisha mifumo yote ya maisha :-
+                    Ili kupata vijana waliokuwa bora na wanaomcha Allah(s.w) malezi yanachukua nafasi kubwa katika kuwajenga watoto wetu.
+                    Kuanzia katika umri mdogo kabisa watoto wetu wanafundishwa na kulelewa katika malezi, misingi na taratibu za dini ya kiislamu ili waweze 
+                    kuyazoea maisha hayo na kuyafanya ni sehemu ya maisha yao ya kila siku. <br />
+                    Malezi hayo yanahusisha mifumo yote ya maisha :-
                   </Card.Text>
                   <Card.Text>
                     <ul>
                       <li>Maisha ya kiislamu</li>
                       <li>Ibada za kiislamu</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
+                      <li>Mavazi ya kiislamu wawapo shuleni na nyumbani</li>
+                      <li>Tamaduni mbali mbali za kiislamu</li>
+                      <li>Dua na Adh-kar mbalimbali</li>
                     </ul>
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
-
+          <hr />
           <Row>
             <Col>
               <Card>
                 <Card.Body>
-                  <Card.Title>Michezo</Card.Title>
+                  <Card.Title>Masomo</Card.Title>
                   <Card.Text>
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
+                      Katika kutoa elimu na ujuzi masomo yote yana umuhimu sawa ili kujenga jamii iliyoelimika na yenye <strong>kumuogopa na Kumcha Mwenyezi Mungu kikweli kweli</strong> <br />
+                      Hivyo shule zetu zinafundisha <strong><i>Masomo ya dini</i></strong> kama masomo mengine na kwa kufuata mitaala inayotambuliwa na serikali katika utoaji a elimu.
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -84,15 +88,25 @@ function PrimaryScreen() {
           <Card className="mb-2" border="warning">
             <Card.Header>Masomo wanayojifunza</Card.Header>
             <Card.Body>
-              
-              <ol>
-                <li>Vitendo vya kuhesabu</li>
-                <li>Vitendo vya kuwasiliana</li>
-                <li>Kutunza mazingira</li>
-                <li>Kutunza Afya</li>
-                <li>Kumudu stadi za kisanii</li>
-                <li>Malezi ya kiislamu</li>
-              </ol>
+              <Row>
+                <Col>
+                  <ol>
+                    <li>Elimu ya dini ya Kiislamu</li>
+                    <li>Lugha ya Kiarabu</li>
+                    <li>Qur-an Tajweed</li>
+                    <li>Hifdhul Qur-an</li>
+                  </ol>
+                </Col>
+                <Col>
+                  <ol>
+                    <li>Hesabu</li>
+                    <li>Sayansi</li>
+                    <li>English language</li>
+                    <li>Kiwswahili</li>
+                    <li>Maarifa ya Jamiii(Uraia, Historia na Jiografia)</li>
+                  </ol>
+                </Col>
+              </Row>
             </Card.Body>
             
           </Card>
