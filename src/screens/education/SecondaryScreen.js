bcfe93f54502b0c702a3e901c942ca5a94ee6bcc
mwinamijr <mwinamijr@gmail.com>
2021-07-03 10:22:12 +0300

    modified secondary school page contents

--- src/screens/education/SecondaryScreen.js
+++ src/screens/education/SecondaryScreen.js
@@ -6,15 +6,22 @@ function SecondaryScreen() {
     return (
       <div>
         <div className="px-4 my-5 text-center border-bottom">
-          <h3 className="display-4 fw-bold">Hayatul Islamiya Scondary School(HIHS)</h3>
-          <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <h3 className="display-4 fw-bold">Hayatul Islamiya Secondary School(HIHS)</h3>
+          <div className="col-lg-8 mx-auto">
+            <p className="lead mb-4">
+              Hayatul Islamiya Secondary School ni shule inayotoa elimu bora kwa gharama nafuu kabisa. Kama zilivyo shule zote za Hayatul Islamiya, shule hii pia inatoa 
+              elimu na mafunzo yote kwa kuzingatia <b>misingi na taratibu za dini ya kiislamu.</b> <br />
+            </p>
+            <p className="text-white rounded bg-dark">
+                <h5 className="display-4 fw-bold">Form One - Form Six</h5>
+                <h5 className="text-center text-danger">Shule ni ya Bweni na kutwa</h5>
+              </p>
           </div>
         </div>
           <Row>
-            <Col>
+            <Col md={4}>
               <Card>
-                <Card.Img variant="top" src="http://127.0.0.1:8000/media/nursery/20201128_091608.jpg" />
+                <Card.Img variant="top" src="http://127.0.0.1:8000/images/nursery/20201128_091608.jpg" />
                 <Card.Body>
                 <Card.Title>Card title that wraps to a new line</Card.Title>
                 <Card.Text>
@@ -24,48 +31,48 @@ function SecondaryScreen() {
                 </Card.Body>
               </Card>
             </Col>
-
-            <Col>
+              <hr />
+            <Col md={4}>
               <Card>
                 <Card.Body>
                   <Card.Title>Malezi</Card.Title>
                   <Card.Text>
-                      Kuanzia katika umri mdogo kabisa watoto wetu wanafundishwa malezi katika misingi na taratibu za dini ya kiislamu. 
-                      Malezi hayo yanahusisha mifumo yote ya maisha :-
+                    Ili kupata vijana waliokuwa bora na wanaomcha Allah(s.w) malezi yanachukua nafasi kubwa katika kuwajenga watoto wetu.
+                    Kuanzia katika umri mdogo kabisa watoto wetu wanafundishwa na kulelewa katika malezi, misingi na taratibu za dini ya kiislamu ili waweze 
+                    kuyazoea maisha hayo na kuyafanya ni sehemu ya maisha yao ya kila siku. <br />
+                    Malezi hayo yanahusisha mifumo yote ya maisha :-
                   </Card.Text>
                   <Card.Text>
-                    <ul>
+                    <ul className="text-info">
                       <li>Maisha ya kiislamu</li>
                       <li>Ibada za kiislamu</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
-                      <li>Dua mbalimbali</li>
+                      <li>Mavazi ya kiislamu wawapo shuleni na nyumbani</li>
+                      <li>Tamaduni mbali mbali za kiislamu</li>
+                      <li>Dua na Adh-kar mbalimbali</li>
                     </ul>
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
-
+          <hr />
           <Row>
-            <Col>
+            <Col md={4}>
               <Card>
                 <Card.Body>
                   <Card.Title>Michezo</Card.Title>
                   <Card.Text>
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
-                      Ili kuwajenga watoto wetu katika afya njema wanapata fursa ya kushiriki katika michezo mablimbali ili kuweza kukuza uwezo 
-                      wao wa kushirikia na wenzao katika kufanya shughuli mbalimbali.
+                  Katika kutoa elimu na ujuzi masomo yote yana umuhimu sawa ili kujenga jamii iliyoelimika na yenye <strong>kumuogopa na Kumcha Mwenyezi Mungu kikweli kweli</strong> <br />
+                      Hivyo shule zetu zinafundisha <strong><i>Masomo ya dini</i></strong> kama masomo mengine na kwa kufuata mitaala inayotambuliwa na serikali katika utoaji a elimu.
+                  </Card.Text>
+                  <Card.Text className="text-success">
+                    Masomo ya dini yanapewa kipaumbele cha hali ya juu ili kuwaandaa watoto wetu kama raia wema wanaomugopa na kumcha Allah (s.w.)
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
             
-              <Col>
+              <Col md={4}>
                 <Card>
                   
                   <Card.Body>
@@ -75,24 +82,68 @@ function SecondaryScreen() {
                       additional content. This content is a little bit longer.
                   </Card.Text>
                   </Card.Body>
-                  <Card.Img variant="top" src="http://127.0.0.1:8000/media/nursery/20201128_091608.jpg" />
+                  <Card.Img variant="top" src="http://127.0.0.1:8000/images/nursery/20201128_091608.jpg" />
                 </Card>
               </Col>
               
           </Row>
           <br />
           <Card className="mb-2" border="warning">
-            <Card.Header>Masomo wanayojifunza</Card.Header>
+            <Card.Header>Masomo wanayojifunza O-Level</Card.Header>
             <Card.Body>
-              
-              <ol>
-                <li>Vitendo vya kuhesabu</li>
-                <li>Vitendo vya kuwasiliana</li>
-                <li>Kutunza mazingira</li>
-                <li>Kutunza Afya</li>
-                <li>Kumudu stadi za kisanii</li>
-                <li>Malezi ya kiislamu</li>
-              </ol>
+              <Row>
+                <Col md={4}>
+                  <ol>
+                    <li>Elimu ya dini ya Kiislamu</li>
+                    <li>Lugha ya Kiarabu</li>
+                    <li>Qur-an Tajweed</li>
+                    <li>Hifdhul Qur-an</li>
+                  </ol>
+                </Col>
+                <Col md={4}>
+                  <ol>
+                    <li>Basic Mathematics</li>
+                    <li>Physics</li>
+                    <li>Chemistry</li>
+                    <li>Biology</li>
+                  </ol>
+                </Col>
+                <Col md={4}>
+                  <ol>
+                    <li>English</li>
+                    <li>Kiswahili</li>
+                    <li>History</li>
+                    <li>Geography</li>
+                    <li>Civics</li>
+                  </ol>
+                </Col>
+              </Row>
+            </Card.Body>
+            
+          </Card>
+          <br />
+          <Card className="mb-2" border="info">
+            <Card.Header>Combination za A-Level</Card.Header>
+            <Card.Body>
+              <Row>
+                <Col md={4}>
+                  <ol>
+                    <li>PCM</li>
+                    <li>PCB</li>
+                    <li>PGM</li>
+                    <li>EGM</li>
+                  </ol>
+                </Col>
+                <Col md={4}>
+                  <ol>
+                    <li>HGL</li>
+                    <li>HKL</li>
+                    <li>HGK</li>
+                    <li>HGE</li>
+                    <li>KLA</li>
+                  </ol>
+                </Col>
+              </Row>
             </Card.Body>
             
           </Card>
