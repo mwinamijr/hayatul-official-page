50784f323dfdb4c87ccd0daf89277dea26f86dc7
Athumani Mwinami <mwinamijr@gmail.com>
2021-07-12 22:47:40 +0300

    Update userActions.js

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -49,7 +49,7 @@ export const login = (email, password) => async (dispatch) => {
         }
 
         const { data } = await axios.post(
-            'http://127.0.0.1:8000/api/users/login/',
+            'https://www.hayatul.herokuapp.com/api/users/login/',
             { 'username': email, 'password': password },
             config
         )
