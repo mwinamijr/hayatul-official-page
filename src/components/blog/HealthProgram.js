9efd96b8a67851ebcdb94b4579ffa4455228e766
mwinamijr <mwinamijr@gmail.com>
2021-07-13 22:06:21 +0300

    added health program blog details

--- src/components/blog/HealthProgram.js
+++ src/components/blog/HealthProgram.js
@@ -6,16 +6,19 @@ function HealthProgram() {
     return (
         <div>
             <div className="p-4 p-md-5 mb-4 text-black rounded bg-light">
-            <Link to="/blog/" className='btn btn-light my-3'>Go Back</Link>
+            <Link to="/blogu/" className='btn btn-light my-3'>Go Back</Link>
             <div className="row">
                 <div className="col-md-1"></div>
                 <div className="col-md-10 px-0">
-                    <h1 className="display-4 fst-italic">Ujenzi wa zahanati</h1>
-                    <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
-                    Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
-                    Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
-                    Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
-                    Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.
+                    <h1 className="display-4 fst-italic">Hayatul Islamiya Health Center</h1>
+                    <p className="lead my-3">Kwa takriban miaka 23 sasa Taasisi ya Hayatul Islamiya Complex imekuwa ikitoa huduma mbalimabli kwa jamii zikiwemo huduma za maji na Elimu, kama ilivyo kwa malengo ya 
+                      kuanzishwa kwa taasisi hii, <b>"kuihudumia jamii"</b>. 
+                      </p>
+                      <p>
+                      <h4>Huduma ya Afya</h4>
+                      Baada ya kuonyesha uzoefu mkubwa katika kuihudumia jamii katika nyanja za elimu, kwa sasa Tumeanzisha ujenzi wa zahanati ya kisasa itakayokuwa inatoa huduma zote muhimu 
+                      na kwa gharama nafuu kwani huduma ya Afya inapaswa kupatikana kwa watu wote. <b><i>Utoaji wa huduma hii katika kituo chetu utazingatia mafundisho na taratibu za dini 
+                          ya kiislamu kama inavyoelekezwa katika Qur-an na sunnah.</i></b>
                     </p>
                     
                 </div>
